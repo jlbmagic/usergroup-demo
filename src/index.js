f87c28f7792740e78184f82a0728c856dffaed30
Jeremy Brown <jlbmagic@gmail.com>
2022-03-04 22:33:20 -0500

    got the c3 chart working with fake data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 //Here we're importing items we'll need. You can add other imports here.
+import c3 from "c3";
+import { sampleData } from "./sampleData";
+console.log(sampleData);
 
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
+window.loadChart = function (array) {
+  const columns = JSON.parse(array);
+  console.log(columns);
+  var chart = c3.generate({
+    bindto: "#chart",
+    data: {
+      type: "line",
+      columns: columns,
+    },
+  });
 };
-
-//
---
+++ src/sampleData.js
@@ -0,0 +1,4 @@
+export const sampleData = [
+  ["Apples", 230, 200, 100, 400, 150, 250],
+  ["Peaches", 550, 20, 410, 40, 15, 25],
+];
